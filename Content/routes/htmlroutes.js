1bec3d3fb0db2363b77c7c025bdd6e1e2a45cf6a
TKShadowBlade <katanablack85@gmail.com>
2021-10-11 01:58:49 -0400

    routing scripts

---
+++ Content/routes/htmlroutes.js
@@ -0,0 +1,22 @@
+// Pull In Dependencies Here
+// pull in path package to get the right file path for html
+const path = require('path');
+
+// Routing Code Goes Here
+
+// GET requests for notes.html and index.html
+module.exports = (app) => {
+    app.get ('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/notes.html'))
+    });
+
+    app.get ('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'))
+    });
+
+    // Default route to index file 
+    app.get ('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'))
+    })
+}
+
