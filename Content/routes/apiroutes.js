565d7320609516739caac631969f6d6b4dfc9181
TKShadowBlade <katanablack85@gmail.com>
2021-10-11 23:47:00 -0400

    Route files updated

--- Content/routes/apiroutes.js
+++ Content/routes/apiroutes.js
@@ -1,5 +1,34 @@
 const fs = require ('fs');
 
 module.exports = (app) => {
+    app.get ('/api/notes', (req, res) => {
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            databaseData = JSON.parse(data);
+            res.send(databaseData)
+        });
+    });
 
-}
+    app.post ('/api/notes', (req, res) => {
+        const userNote = req.body;
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            databaseData = JSON.parse (data);
+            databaseData.push (userNote);
+            let number = 1;
+            databaseData.forEach ((note, index) => {
+                note.id = number;
+                number++;
+                return databaseData;
+            });
+              console.log (databaseData);
+              
+              stringData = JSON.stringify(databaseData);
+
+              fs.writeFile('./db/db.json', stringData, (err, data) => {
+                  if (err) throw err;
+              });
+        });
+        res.send ('Thanks for your note!')
+    });
+}   
--- Content/routes/htmlroutes.js
+++ Content/routes/htmlroutes.js
@@ -7,16 +7,16 @@ const path = require('path');
 // GET requests for notes.html and index.html
 module.exports = (app) => {
     app.get ('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/notes.html'))
+        res.sendFile(path.join(__dirname, '../public/notes.html'))
     });
 
     app.get ('/', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/index.html'))
+        res.sendFile(path.join(__dirname, '../public/index.html'))
     });
 
     // Default route to index file 
     app.get ('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/index.html'))
+        res.sendFile(path.join(__dirname, '../public/index.html'))
     })
 }
 
