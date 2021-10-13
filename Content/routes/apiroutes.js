9a81814ce53d59987cec65054ecfc93a85fb9f1a
TKShadowBlade <katanablack85@gmail.com>
2021-10-12 23:40:54 -0400

    updates to route js syntax

--- Content/routes/apiroutes.js
+++ Content/routes/apiroutes.js
@@ -1,34 +1,26 @@
-const fs = require ('fs');
+const router = require ('express').Router();
+const store = require ('../db/store');
 
-module.exports = (app) => {
-    app.get ('/api/notes', (req, res) => {
-        fs.readFile('./db/db.json', (err, data) => {
-            if (err) throw err;
-            databaseData = JSON.parse(data);
-            res.send(databaseData)
-        });
-    });
+// where all notes from the database go
+router.get ('/notes', (req, res) => {
+    store
+    .noteGet()
+    .then (notes => res.json (notes))
+    .catch (err => res.status(500).json(err));
+});
 
-    app.post ('/api/notes', (req, res) => {
-        const userNote = req.body;
-        fs.readFile('./db/db.json', (err, data) => {
-            if (err) throw err;
-            databaseData = JSON.parse (data);
-            databaseData.push (userNote);
-            let number = 1;
-            databaseData.forEach ((note, index) => {
-                note.id = number;
-                number++;
-                return databaseData;
-            });
-              console.log (databaseData);
-              
-              stringData = JSON.stringify(databaseData);
+router.post ('/notes', (req, res) => {
+    store
+    .noteAdd(req.body)
+    .then((note) => res.json(note))
+    .catch (err => res.status(500).json(err));
+});
 
-              fs.writeFile('./db/db.json', stringData, (err, data) => {
-                  if (err) throw err;
-              });
-        });
-        res.send ('Thanks for your note!')
-    });
-}   
+router.delete ('/notes/:id', (req, res) => {
+    store
+    .noteRemove(req.params.id)
+    .then(() => res.json({ ok: true}))
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
--- Content/server.js
+++ Content/server.js
@@ -1,5 +1,7 @@
 // pulls in express npm package
 const express = require('express');
+const apiRoutes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlroutes');
 
 // activates express for use in coding
 const app = express();
@@ -8,14 +10,14 @@ const app = express();
 const PORT = process.env.port || 3003;
 
 // Middleware data parsing
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-require('./routes/htmlroutes')(app);
-require('./routes/apiroutes')(app);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Opens the port to accept requests
 app.listen(PORT, () => {
-    console.log ('Listening on port: ${PORT)');
+    console.log (`Listening on port: ${PORT}`);
 })
